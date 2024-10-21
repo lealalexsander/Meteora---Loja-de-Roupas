7e9afc32ff509e42e6f48fada64fedcff27da76b
Alexsander Leal <alexsanderunr@gmail.com>
2024-10-21 15:05:16 -0300

    adição de carrossel

--- js/imprimirUmDeCadaCategoria.js
+++ js/imprimirUmDeCadaCategoria.js
@@ -8,7 +8,7 @@ function adicionarProduto(camiseta) {
     imagens: camiseta.imagens
   });
 }
-
+ 
 export function imprimirUmDeCadaCategoria(produtos) {
   const row = document.querySelector("#produtos");
 
@@ -39,8 +39,31 @@ export function imprimirUmDeCadaCategoria(produtos) {
         </div>
       `;
 
+      const gerarHTMLCarrossel = (imagens, nome) => {
+        if (typeof imagens !== "object" || Array.isArray(imagens)) {
+          console.error("as imagens devem ser enviadas como um objeto")
+          return ""
+        }
+
+        const urlsImagens = [imagens.mobile, imagens.tablet, imagens.desktop];
+
+        return `
+        <div id="carrossel${nome}" class="slideshow-container">
+          ${urlsImagens.map((urlImagem, indice) => `
+            <div class="meusSlides-${nome} fade">
+            <img src=${urlImagem} alt="imagem ${indice}" style="width: 100%">
+            </div>
+          `)}
+          <a class="anterior" id="anterior-${nome}">&#10094;</a>
+          <a class="proxima" id="proxima-${nome}">&#10095;</a>
+        </div>
+        `
+      }
+
       card.innerHTML = images + cardBody;
-      card.className = "card card-animado";
+      card.className = "card card-animado"
+
+      const carrossel = gerarHTMLCarrossel(produto.imagens, produto.nome.replace(/\s+/g, "-"))
 
       const modalContent = `
         <div class="modal-content">
@@ -52,8 +75,7 @@ export function imprimirUmDeCadaCategoria(produtos) {
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <img class="modal-imagem" src="${produto.imagens.desktop}" alt="${produto.nome
-        }">
+            ${carrossel}
             <div>
               <h3>${produto.nome}</h3>
               <p class="modal-description">${produto.descricao}</p>
@@ -89,24 +111,24 @@ export function imprimirUmDeCadaCategoria(produtos) {
               <hr class="divider-secondary">
 
               <p><b>Tamanho</b></p>
-              <form>
-                <label class="radio-container" for="tamanho-p">
-                  <input type="radio" name="tamanho-p" value="P" id="tamanho-p"/>
-                  <span class="checkmark"></span>
-                  <span class="radio-label">P</span>
-                </label>
+               <form>
+              <label class="radio-container" for="tamanho-p">
+                <input type="radio" name="tamanho-p" value="P" id="tamanho-p"/>
+                <span class="checkmark"></span>
+                <span class="radio-label">P</span>
+              </label>
 
-                <label class="radio-container" for="tamanho-m">
+                   <label class="radio-container" for="tamanho-m">
                 <input type="radio" name="tamanho-m" value="M" id="tamanho-m"/>
                 <span class="checkmark"></span>
                 <span class="radio-label">M</span>
               </label>
 
-                <label class="radio-container" for="tamanho-g">
-                <input type="radio" name="tamanho-g" value="G" id="tamanho-pg"/>
+                   <label class="radio-container" for="tamanho-g">
+                <input type="radio" name="tamanho-g" value="G" id="tamanho-g"/>
                 <span class="checkmark"></span>
                 <span class="radio-label">G</span>
-                </label>
+              </label>
               </form>
             </div>
           </div>
@@ -128,29 +150,50 @@ export function imprimirUmDeCadaCategoria(produtos) {
 
       const botao = document.querySelector(`#adicionar-btn-${produto.nome.replace(/\s+/g, "-")}`);
       botao.addEventListener("click", () => adicionarProduto(produto));
+
+      let indiceSlide = 1;
+      const mostrarSlides = (numero) => {
+        const slides = document.querySelectorAll(`.meusSlides-${produto.nome.replace(/\s+/g, "-")}`)
+        if (numero > slides.length) indiceSlide = 1;
+        if (numero < 1) indiceSlide = slides.length
+        slides.forEach(slide => slide.style.display = "none");
+        slides[indiceSlide - 1].style.display = "block";
+      }
+
+      const maisSlides = (numero) => mostrarSlides(indiceSlide += numero);
+
+      document.getElementById(`anterior-${produto.nome.replace(/\s+/g, "-")}`).onclick = () => {
+        maisSlides(-1);
+      }
+      document.getElementById(`proxima-${produto.nome.replace(/\s+/g, "-")}`).onclick = () => {
+        maisSlides(1);
+      }
+
+      mostrarSlides(indiceSlide);
+
     }
+
   }
 
-
-
-  function elementoEstaNoviewport(elemento) {
+  function elementoEstaNoViewport(elemento) {
     const retangulo = elemento.getBoundingClientRect();
     return (
       retangulo.top >= 0 &&
-      regangulo.left >= 0 &&
-      retangulo.bottom <= (window.innerHeight || document.documentElement.clienteHeight) &&
+      retangulo.left >= 0 &&
+      retangulo.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
       retangulo.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    )
   }
 
-  function elementoEstaNaSecaoDosCards(){
-    const secaoDosCards = document.getElementById ('produtos');
-    const retanguloDoElemento = elemento.getBoundingClientRect();
-    const retanguloSecao = secaoDosCards.getBoundingClientRect();
 
-    return(
-      retanguloDoElemento.top >= reganguloSecao.top &&
-      retanguloDoElemento.bottom >= retanguloSecao.bottom
+  // Função para verificar se um elemento está dentro da seção dos cards
+  function elementoEstaNaSecaoDosCards(elemento) {
+    const secaoDosCards = document.getElementById('produtos'); // Identificador da seção dos cards
+    const retanguloElemento = elemento.getBoundingClientRect();
+    const retanguloSecao = secaoDosCards.getBoundingClientRect();
+    return (
+      retanguloElemento.top >= retanguloSecao.top &&
+      retanguloElemento.bottom <= retanguloSecao.bottom
     );
   }
 
@@ -158,13 +201,21 @@ export function imprimirUmDeCadaCategoria(produtos) {
     const cards = document.querySelectorAll(".card");
 
     cards.forEach((card) => {
-      if (elementoEstaNaSecaoDosCards(card) && elementoEstaNoviewport(card)) {
+      if (elementoEstaNaSecaoDosCards(card) && elementoEstaNoViewport(card)) {
         card.classList.add("fade-in")
       }
-    })  
-  }   
-  
-  verificarVisibilidadeDosCards();
-  window.addEventListener("scroll", verificarVisibilidadeDosCards);
+    })
+  }
 
+  verificarVisibilidadeDosCards();
+  window.addEventListener("scroll", verificarVisibilidadeDosCards)
 }
+
+
+
+
+
+
+
+
+
